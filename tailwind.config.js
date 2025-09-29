03405946f5b4e2c018c5029647a415de206f59eb
Cahrinz <ritacassiagfoentes@gmail.com>
2025-09-29 01:25:22 -0300

    remove pomodoro + configs tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './pomodoro.html', './src/**/*.{js,html}'],
+  content: ['./index.html', './pomodoro-simples.html', './src/**/*.{js,html}'],
   darkMode: 'class',
   theme: {
     extend: {}
